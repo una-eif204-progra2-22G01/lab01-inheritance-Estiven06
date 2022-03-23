6f972e794d41a1a44c13f61578081cfd8cdcce32
Estiven06 <kirven08.epa@gmail.com>
2022-03-22 19:18:31 -0600

    Cambio final

--- src/Lab01Inheritance.cpp
+++ src/Lab01Inheritance.cpp
@@ -6,11 +6,14 @@ int main() {
     std::cout << "Welcome to the UNA!" << std::endl;
 
     Derived1 derived1;
+    derived1.setName("Mario");
     derived1.do_something();
 
     Derived2 derived2;
+    derived2.setName("Luigi");
     derived2.do_something();
 
-    return 0;
+    cout<<"to string derived1"<<derived1<<endl;
+    cout<<"to string derived2"<<derived2<<endl;
 
 }  // end function main
